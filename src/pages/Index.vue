bddc0462f47588cd4104a7d145f063f84609af63
Klaudia <khturowska@gmail.com>
2019-06-08 12:01:29 +0200

    Some trailing spaces removed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,12 +3,11 @@
     <q-page padding>
     <div class="fixed-center text-center">
     <h1 class="text-center text-bold">Welcome to BRAINSTORM</h1>
-    <h2 class="caption text-center">Answer or create a quiz you like</h2>    <p>
-    </p>
+    <h2 class="caption text-center">Answer or create a quiz you like</h2>
+    <p></p>
     <q-btn class="flex inline shadow-box flex-center text-bold" @click="$router.push('/randomquiz')">Play random quiz</q-btn>
     <q-btn class="flex inline shadow-box flex-center text-bold" @click="$router.push('/addquiz')">Add a quiz</q-btn>
     <q-btn class="flex inline shadow-box flex-center text-bold" @click="$router.push('/choosequiz')">Choose quiz</q-btn>
-
   </div>
     </q-page>
     </q-page>
